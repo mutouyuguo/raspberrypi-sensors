0da714162a672da2453c2e83e27a73fe116748fd
mutouyuguo <mutouyuguo@gmail.com>
2022-09-09 15:30:43 +0100

    distance sensor

---
+++ distance_sensor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+PIN_TRIGGER = 7
+PIN_ECHO = 11
+
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
+
+GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+print("Waiting for sensor to settle")
+
+try:
+    while True:
+        print("Calculating distance")
+        
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+        
+        time.sleep(0.00001)
+        
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+        
+        while GPIO.input(PIN_ECHO)==0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO)==1:
+            pulse_end_time = time.time()
+        
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
+        print("Distance:",distance,"cm")
+        time.sleep(1)
+
+finally:
+    GPIO.cleanup()
