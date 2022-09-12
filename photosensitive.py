9346294a5644f2b67f7fa82e1ac0c700a3e52551
mutouyuguo <mutouyuguo@gmail.com>
2022-09-12 14:29:44 +0100

    light on

---
+++ photosensitive.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+channel = 17 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+try:
+    while True:
+        if GPIO.input(channel) == GPIO.LOW:
+            print("light on!")
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
